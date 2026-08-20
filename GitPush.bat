e7fdabfc6677d4124458f2cf574d52306e729858
HahnSimon <sh@fayard.dk>
2026-08-20 14:29:56 +0200

    Dashboard update

---
+++ GitPush.bat
@@ -0,0 +1,12 @@
+@echo off
+
+cd /d "C:\Users\sh\OneDrive - fayardas\John Poul Dues filer - Rørafd\Dashboard Rørværksted\Github\Dashboard"
+
+"C:\Users\sh\AppData\Local\Programs\Git\cmd\git.exe" add .
+
+"C:\Users\sh\AppData\Local\Programs\Git\cmd\git.exe" diff --cached --quiet
+
+if errorlevel 1 (
+    "C:\Users\sh\AppData\Local\Programs\Git\cmd\git.exe" commit -m "Dashboard update"
+    "C:\Users\sh\AppData\Local\Programs\Git\cmd\git.exe" push
+)
